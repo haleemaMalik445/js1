9aa0df8cb2b27c41fec93f67f7080f10d21e7424
haleema-malik445 <haleemamalik@gmail.com>
2023-11-25 08:02:50 -0800

    first commit

---
+++ bimi.js
@@ -0,0 +1,13 @@
+"use strict";
+ var firstname="haleema";
+ console.log(firstname);
+ firstname="bimi";
+ console.log(firstname);
+ 
+let firstName=("saeedakhter");
+console.log(firstName[0]);
+console.log(firstName[6]);
+//length
+console.log(firstName.length);
+console.log(firstName[firstName.length-1]);
+console.log(firstName[firstName.length-3]);
